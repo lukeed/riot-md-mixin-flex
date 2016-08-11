677c8987001b5c4bc596c59658f7eb0cc52187c8
Luke Edwards <luke.edwards05@gmail.com>
2016-08-10 17:20:04 -0700

    add mixin body

---
+++ index.js
@@ -0,0 +1,16 @@
+function capitalize(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+module.exports = {
+	init: function () {
+		var self = this;
+
+		self.on('mount', function () {
+			var el = self.root.firstElementChild;
+			['flex', 'dflex', 'flexrow'].forEach(function (attr) {
+				(capitalize(attr) in self.opts) && el.setAttribute(attr, self.opts[attr] || '');
+			});
+		});
+	}
+};
